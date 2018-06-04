8a313cf56f543c12b6f0ed17399c77b25559e779
esquinas <esquinas.enrique@gmail.com>
2018-06-04 20:10:26 +0200

    Make CSRF token more secure checking user's emails.

--- handlers/topic.py
+++ handlers/topic.py
@@ -8,10 +8,14 @@ from models.topic import Topic
 
 class TopicAddHandler(BaseHandler):
     def get(self):
+        logged_user = users.get_current_user()
+
+        if not logged_user:
+            return self.write("Please login before you're allowed to post a topic.")
 
         csrf_token = str(uuid.uuid4())
 
-        memcache.add(key=csrf_token, value=True, time=600)
+        memcache.add(key=csrf_token, value=logged_user.email(), time=600)
 
         context = {
             "csrf_token": csrf_token
@@ -20,16 +24,16 @@ class TopicAddHandler(BaseHandler):
         return self.render_template('topic_add.html', params=context)
 
     def post(self):
-        csrf_token = self.request.get('csrf-token')
-        mem_token = memcache.get(key=csrf_token)
-
-        if not mem_token:
-            return self.write('This website is protected against CSRF attacks :P')
-
         logged_user = users.get_current_user()
 
         if not logged_user:
             return self.write('Please login to be allowed to post a new topic.')
+
+        csrf_token = self.request.get('csrf-token')
+        mem_token = memcache.get(key=csrf_token)
+
+        if not mem_token or mem_token != logged_user.email():
+            return self.write('This website is protected against CSRF attacks :P')
 
         title_value = self.request.get('title')
         text_value = self.request.get('text')
