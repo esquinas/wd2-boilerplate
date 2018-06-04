ccab1229d3060ecaed97a06a266615d2547d1b9c
esquinas <esquinas.enrique@gmail.com>
2018-06-04 20:37:54 +0200

    List topics.

--- handlers/home.py
+++ handlers/home.py
@@ -1,7 +1,13 @@
 from handlers.base import BaseHandler
-
+from models.topic import Topic
 
 class HomeHandler(BaseHandler):
 
     def get(self):
-        return self.render_template("home.html")
+        topics = Topic.query(Topic.deleted == False).fetch()
+
+        context = {
+            'topics': topics,
+        }
+
+        return self.render_template("home.html", params=context)
