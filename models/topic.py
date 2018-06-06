00aed84ca8a8452d3a84ee01612f13bd1ac6b4ac
esquinas <esquinas.enrique@gmail.com>
2018-06-06 20:56:56 +0200

    TODO: Homeworks.

--- models/topic.py
+++ models/topic.py
@@ -8,3 +8,8 @@ class Topic(ndb.Model):
     created = ndb.DateTimeProperty(auto_now_add=True)
     updated = ndb.DateTimeProperty(auto_now=True)
     deleted = ndb.BooleanProperty(default=False)
+
+    # Create Topics here.
+
+    # Delete Topics here if admin or author. Also delete its comments.
+    # users.is_current_user_admin()
